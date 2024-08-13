a3be1dcfb268167f104ef8339f7bdd708d4378c7
jpedro002 <joaopsilvavolei@gmail.com>
2024-08-13 14:57:48 -0300

    fix: cors again

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,45 +8,26 @@ export const config = {
 
 export async function middleware(req: NextRequest) {
 	const pathname = req.nextUrl.pathname
-
 	const session = await AuthService.isSessionValid()
 
-	// console.log(
-	// 	'session',
-	// 	session,
-	// 	Math.random(),
-	// 	pathname.split('/').includes('/admin'),
-	// 	123,
-	// )
+	const isAdminPath = () => pathname.split('/').includes('admin')
+
+	const isApiPath = () => pathname.split('/').includes('api')
+
+	const isAuthPath = () =>
+		pathname === '/auth' ||
+		pathname === '/api/auth/login' ||
+		pathname === '/api/auth/logout'
 
 	console.log('pathname', pathname)
 
-	if (!session && pathname.split('/').includes('admin')) {
+	if (!session && isAdminPath()) {
 		return NextResponse.redirect(new URL('/auth', req.url))
 	} else if (session && pathname === '/auth') {
 		return NextResponse.redirect(new URL('/admin/plantel', req.url))
-	} else if (pathname === '/auth' || '/api/auth/login' || '/api/auth/logout') {
+	} else if (isAuthPath()) {
 		return NextResponse.next()
-	} else if (!session && pathname.split('/').includes('api')) {
+	} else if (!session && isApiPath()) {
 		return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
 	}
-
-	// retrieve the current response
-	const res = NextResponse.next()
-
-	// add the CORS headers to the response
-	res.headers.append('Access-Control-Allow-Credentials', 'true')
-	res.headers.append('Access-Control-Allow-Origin', env.APP_URL || '*') // replace this your actual origin
-	res.headers.append(
-		'Access-Control-Allow-Methods',
-		'GET,DELETE,PATCH,POST,PUT',
-	)
-	res.headers.append(
-		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-	)
-
-	return res
 }
-
-// specify the path regex to apply the middleware to
